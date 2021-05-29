7336ced97ea470bbfcc829976de99628ff70973a
Ruslansmu <lulka755@gmail.com>
2021-05-29 17:37:22 +0300

    fix index
    removed duplicate index paths in Main.class(indexBuild)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.io.*;
+import java.lang.reflect.AnnotatedArrayType;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -11,33 +12,35 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
 import static com.company.Server.server;
 
 public class Main {
-
+    public static Map<String,ArrayList<String>> multiMap = new HashMap<String,ArrayList<String>>();
+    public static ArrayList<String> emptyArr = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
-        //readFile();
-        server();
+        readFile();
+        // server();
+        find("ruslan");
     }
 
-
     public static void readFile(){
+
         long m = System.currentTimeMillis();
-        File dir = new File("/Users/ruslanl/Documents/6semester/Паралельки/course/datasets/aclImdb/train/neg");
-        Map<String,ArrayList<String>> multiMap = new HashMap<String,ArrayList<String>>();
+        File dir = new File("/Users/ruslanl/Documents/6semester/Паралельки/course/testSet");
+
         File[] files = dir.listFiles();
         for (int i = 0; i < files.length; i++){
-            //System.out.println(files[i]);
-
+            System.out.println(files[i]);
             try (BufferedReader br = new BufferedReader(new FileReader(files[i]))) {
                 //чтение построчно
                 String s;
+                //System.out.println(files[i]);
                 while ((s = br.readLine()) != null) {
                     s = refactText(s);
                    // System.out.println(s);
-                    tokenBuild(s, multiMap, files[i]);
+
+                    indexBuild(s, files[i]);
 
                 }
             } catch (IOException ex) {
@@ -49,19 +52,25 @@ public class Main {
         System.out.println(System.currentTimeMillis() - m);
     }
 
-    public static String tokenBuild(String word, Map<String,ArrayList<String>> multiMap, File file){
-        ArrayList<String> listFile = new ArrayList<>();
+    public static String indexBuild(String word, File file){
+        System.out.println(file.getName());
         String[] words = word.split(" ");
         for(String wordIn : words){
-            //System.out.println(wordIn);
-            String docID = file.getParent()+file.getName();
+            System.out.println(wordIn);
+            String docID = file.getParent()+ "/" +file.getName();
+            System.out.println(docID);
+
             if (multiMap.containsKey(wordIn)){
+                if (multiMap.get(wordIn).contains(docID)){
+                    continue;
+                }
                 multiMap.get(wordIn).add(docID);
             }
             else {
-                multiMap.put(wordIn, listFile);
+                multiMap.put(wordIn, new ArrayList<>());
                 multiMap.get(wordIn).add(docID);
             }
+            System.out.println("test");
         }
 
         return word;
@@ -75,5 +84,24 @@ public class Main {
         return text;
     }
 
+    public static void find(String phrase){
+        String[] words = phrase.split("\\W+");
+        HashSet<String> res = new HashSet<String>(multiMap.get(words[0]));
+
+        //System.out.println(multiMap.get(words[0]));
+        for(String word: words){
+            res.retainAll(multiMap.get(word));
+        }
+      //  System.out.println(res);
+
+        if(res.size()==0) {
+            System.out.println("Not found");
+            return;
+        }
+        System.out.println("Found in: ");
+        for(String num : res){
+            //System.out.println("\t"+ emptyArr.stream().filter(s -> s.contains(num)));
+        }
+    }
 
 }
